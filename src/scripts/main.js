3929eba89bd6cdd80abed9946ea2ab56ff54d386
Sangkook Lee <sangkook.lee@solarsdev.com>
2022-01-24 15:19:37 +0900

    feat: Add game hud update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,25 @@
 
 const playBtn = document.querySelector('.game-info__play-btn');
 const gameField = document.querySelector('.game-field');
+const gamePopup = document.querySelector('.game-popup');
+const redoBtn = document.querySelector('.game-popup__redo-btn');
+const resultText = document.querySelector('.game-popup__result-text');
+
+// game assets
 const audioBg = new Audio('sound/bg.mp3');
 const audioAlert = new Audio('sound/alert.wav');
 const audioWin = new Audio('sound/game_win.mp3');
 
+// game configs
 const configPlayTime = 10;
 const configCarrotNum = 10;
 const configBugNum = 7;
 
+// game status
+let playing = 0;
 let carrotLeft = 0;
+let gameId = 0;
+let timeLeft = 0;
 
 const createCarrotElement = (x, y) => {
   const carrot = document.createElement('img');
@@ -20,16 +30,20 @@ const createCarrotElement = (x, y) => {
   carrot.style.top = `${y}px`;
 
   carrot.addEventListener('click', () => {
+    if (!playing) {
+      return;
+    }
     const audioCarrotPull = new Audio('sound/carrot_pull.mp3');
     audioCarrotPull.currentTime = 0;
     audioCarrotPull.play();
     gameField.removeChild(carrot);
+
     carrotLeft--;
+    const carrotsIndicator = document.querySelector('.game-info__carrots-left');
+    carrotsIndicator.innerText = carrotLeft;
 
     if (carrotLeft === 0) {
-      audioBg.pause();
-      audioWin.currentTime = 0;
-      audioWin.play();
+      gameWon();
     }
   });
 
@@ -44,15 +58,25 @@ const createBugElement = (x, y) => {
   bug.style.top = `${y}px`;
 
   bug.addEventListener('click', () => {
+    if (!playing) {
+      return;
+    }
     const audioBugPull = new Audio('sound/bug_pull.mp3');
     audioBugPull.currentTime = 0;
     audioBugPull.play();
+    gameLost();
   });
 
   return bug;
 };
 
 const setupGame = () => {
+  // clear game field
+  const gameItems = document.querySelectorAll(
+    '.game-field__carrot,.game-field__bug'
+  );
+  gameItems.forEach((gameItem) => gameField.removeChild(gameItem));
+
   const gameFieldRect = gameField.getBoundingClientRect();
 
   for (let i = 0; i < configCarrotNum; i++) {
@@ -61,8 +85,11 @@ const setupGame = () => {
       Math.random() * (gameFieldRect.height - 80)
     );
     gameField.appendChild(carrot);
-    carrotLeft++;
   }
+
+  carrotLeft = configCarrotNum;
+  const carrotsIndicator = document.querySelector('.game-info__carrots-left');
+  carrotsIndicator.innerText = carrotLeft;
 
   for (let i = 0; i < configBugNum; i++) {
     const bug = createBugElement(
@@ -73,36 +100,79 @@ const setupGame = () => {
   }
 };
 
-const startGame = (btnElement) => {
-  console.log('start game');
-  btnElement.innerHTML = '<i class="fas fa-stop"></i>';
-  btnElement.dataset.playing = '1';
+const gameWon = () => {
+  resultText.innerText = 'YOU WON 🎉';
+  audioWin.currentTime = 0;
+  audioWin.play();
+  endGame();
+};
 
+const gameLost = () => {
+  resultText.innerText = 'YOU LOST 😭';
+  audioAlert.currentTime = 0;
+  audioAlert.play();
+  endGame();
+};
+
+const stopGame = () => {
+  resultText.innerText = 'RESTART';
+  audioAlert.currentTime = 0;
+  audioAlert.play();
+  endGame();
+};
+
+const startGame = () => {
+  gamePopup.style.visibility = 'hidden';
+  playBtn.style.visibility = 'visible';
+  playBtn.innerHTML = '<i class="fas fa-stop"></i>';
   audioBg.currentTime = 0;
   audioBg.play();
   setupGame();
+  playing = 1;
+
+  timeLeft = configPlayTime;
+  const timeIndicator = document.querySelector('.game-info__timer');
+  timeIndicator.innerText = new Date(timeLeft * 1000)
+    .toString()
+    .substring(19, 24);
+
+  gameId = setInterval(() => {
+    timeLeft--;
+    const timeIndicator = document.querySelector('.game-info__timer');
+    timeIndicator.innerText = new Date(timeLeft * 1000)
+      .toString()
+      .substring(19, 24);
+    if (timeLeft < 1) {
+      gameLost();
+    }
+  }, 1000);
 };
 
-const gameWon = () => {};
-
-const endGame = (btnElement) => {
-  console.log('end game');
-  btnElement.innerHTML = '<i class="fas fa-play"></i>';
-  btnElement.dataset.playing = '0';
-
+const endGame = () => {
+  gamePopup.style.visibility = 'visible';
+  playBtn.style.visibility = 'hidden';
+  playBtn.innerHTML = '<i class="fas fa-play"></i>';
   audioBg.pause();
-  audioAlert.play();
+
+  if (gameId) {
+    clearInterval(gameId);
+    gameId = 0;
+  }
+
+  playing = 0;
 };
 
-playBtn.addEventListener('click', (event) => {
-  const btnElement = event.currentTarget;
-  const playing = btnElement.dataset.playing;
+playBtn.addEventListener('click', () => {
   switch (playing) {
-    case '0':
-      startGame(btnElement);
+    case 0:
+      startGame();
       break;
-    case '1':
-      endGame(btnElement);
+    case 1:
+      stopGame();
       break;
   }
+});
+
+redoBtn.addEventListener('click', () => {
+  startGame();
 });
